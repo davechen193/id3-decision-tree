99c139c2eab3b92bae6c2c06cc3189aa32030891
davechen193 <ychendavid@gmail.com>
2022-04-22 12:15:53 +0800

    modify info gain mechanism

--- id3_decision_tree.py
+++ id3_decision_tree.py
@@ -1,4 +1,5 @@
 import numpy as np, pandas as pd
+import matplotlib.pyplot as plt
 from scipy.stats import ks_2samp
 
 class node:
@@ -8,27 +9,18 @@ class node:
         self.feature_index = -1
         self.branches = []
 
-def norm_dist_prob(mean, stddev, x):
-    prob = 1/((stddev) * np.sqrt(2 * np.pi))*np.exp(-1/2 * ((x - mean)/(stddev)) ** 2)
-    prob = 0 if np.isnan(prob) else prob
-    return prob
-
 def calc_entropy(S):
-    mean = np.mean(S)
-    stddev = np.std(S)
-    S_vals = np.unique(S)
-    probs = np.array([norm_dist_prob(mean, stddev, s) for s in S_vals])
+    S_vals, counts = np.unique(S, return_counts=True)
+    probs = np.array([count/S.shape[0] for count, s in zip(counts, S_vals)])
     entropy = 0
     for i in range(probs.shape[0]):
         entropy += -probs[i] * np.log(probs[i])/np.log(2)
     return entropy
 
 def calc_entropy_with_feature(X, label):
-    mean = np.mean(X)
-    stddev = np.std(X)
     entropy = 0
-    label1 = label[X > mean]
-    label2 = label[X <= mean]
+    label1 = label[X > np.median(X)]
+    label2 = label[X <= np.median(X)]
     entropy += (label1.shape[0]/label.shape[0]) * calc_entropy(label1)
     entropy += (label2.shape[0]/label.shape[0]) * calc_entropy(label2)
     return entropy
@@ -47,7 +39,7 @@ def most_common(label):
     return indices[np.argmax(counts.values)]
 
 def id3(root, features, indices, label, min_leaf=10, mode="classifier"):
-    n = root; print(features.shape)
+    n = root
     if mode == "classifier":
         # classification
         pos_ratio = sum(label > 0) / label.shape[0] if label.shape[0] > 0 else np.nan
@@ -60,7 +52,7 @@ def id3(root, features, indices, label, min_leaf=10, mode="classifier"):
         A = indices[0]; ig_max = 0
         for i in indices:
             X = features[:,i]
-            ig = info_gain(X, label); print(ig)
+            ig = info_gain(X, label)
             if ig >= ig_max:
                 A = i; ig_max = ig
     else:
@@ -69,10 +61,10 @@ def id3(root, features, indices, label, min_leaf=10, mode="classifier"):
         A = indices[0]; min_loss = np.inf
         for i in indices:
             vals = features[:,i]
-            vals_mean = np.mean(vals)
+            vals_median = np.median(vals)
             vals_range = [
-                (-np.inf, vals_mean),
-                (vals_mean, np.inf)
+                (-np.inf, vals_median),
+                (vals_median, np.inf)
             ]
             loss = 0; label_subs = []
             for vi in vals_range:
@@ -83,8 +75,6 @@ def id3(root, features, indices, label, min_leaf=10, mode="classifier"):
                 for k in range(i+1, len(label_subs)):
                     l1 = label_subs[j]
                     l2 = label_subs[k]
-                    l1_mean = np.mean(l1); l2_mean = np.mean(l2)
-                    l1_stddev = np.mean(l1); l2_stddev = np.std(l2)
                     if j != k:
                         loss += 1 / ks_2samp(l1, l2).pvalue if (len(l1) > 0 and len(l2) > 0) else 2
             if loss < min_loss:
@@ -92,10 +82,10 @@ def id3(root, features, indices, label, min_leaf=10, mode="classifier"):
     # examine the branches
     n.feature_index = A
     A_vals = features[:,A]
-    A_vals_mean = np.mean(A_vals)
+    A_vals_median = np.median(A_vals)
     A_vals_range = [
-        (-np.inf, A_vals_mean),
-        (A_vals_mean, np.inf)
+        (-np.inf, A_vals_median),
+        (A_vals_median, np.inf)
     ]
     for vi in A_vals_range:
         idx_with_vi = np.argwhere(np.logical_and(features[:,A] > vi[0], features[:,A] <= vi[1]))[:,0]
